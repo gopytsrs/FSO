da89729c5815a917635cc229c5fbecb396fa41ca
Sean <56064050+seangoats@users.noreply.github.com>
2021-12-23 02:57:11 +0800

    Complete 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import PersonForm from './PersonForm';
 import Persons from './Persons';
 import Filter from './Filter';
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
-		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-	]);
-
+	const [persons, setPersons] = useState([]);
 	const [predicate, setPredicate] = useState('');
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const data = await (await axios.get('http://localhost:3001/persons')).data;
+			setPersons(data);
+		};
+		fetchData();
+	}, []);
 
 	const getFilteredPersons = () =>
 		predicate ? persons.filter((person) => person.name.toLocaleLowerCase().includes(predicate.toLocaleLowerCase())) : persons;
