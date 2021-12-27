c407f7ce170208471b768baf2f0ea77122812b41
Sean <56064050+seangoats@users.noreply.github.com>
2021-12-27 10:21:24 +0800

    Change hard code in example to use mongodb

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+
+const dotenv = require('dotenv').config();
+
+const Note = require('./models/note');
+
 app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
@@ -15,57 +20,21 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 
-let notes = [
-	{
-		id: 1,
-		content: 'HTML is easy',
-		date: '2019-05-30T17:30:31.098Z',
-		important: true,
-	},
-	{
-		id: 2,
-		content: 'Browser can execute only Javascript',
-		date: '2019-05-30T18:39:34.091Z',
-		important: false,
-	},
-	{
-		id: 3,
-		content: 'GET and POST are the most important methods of HTTP protocol',
-		date: '2019-05-30T19:20:14.298Z',
-		important: true,
-	},
-];
-
 app.get('/', (req, res) => {
 	res.send(`<h1>Hello World</h1>`);
 });
 
 app.get('/api/notes', (req, res) => {
-	res.json(notes);
+	Note.find({}).then((notes) => res.json(notes));
 });
 
 app.get('/api/notes/:id', (req, res) => {
-	const id = +req.params.id;
-	const note = notes.find((note) => note.id === id);
-	if (note) {
-		res.json(note);
-	} else {
-		res.status(404).end();
-	}
+	Note.findById(req.params.id).then((note) => res.json(note));
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-	const id = Number(req.params.id);
-	notes = notes.filter((note) => note.id !== id);
-
-	res.status(204).end();
+	Note.deleteOne({ id: req.params.id }).then((note) => res.json(note));
 });
-
-const generateId = () => {
-	const maxId = notes.length ? Math.max(...notes.map((n) => n.id)) : 0;
-
-	return maxId + 1;
-};
 
 app.post('/api/notes', (req, res) => {
 	const body = req.body;
@@ -76,16 +45,13 @@ app.post('/api/notes', (req, res) => {
 		});
 	}
 
-	const note = {
+	const note = new Note({
 		content: body.content,
 		important: body.important || false,
 		date: new Date(),
-		id: generateId(),
-	};
+	});
 
-	notes = [...notes, note];
-
-	res.json(note);
+	note.save().then((savedNote) => res.json(savedNote));
 });
 
 const unknownEndpoint = (request, response) => {
@@ -94,7 +60,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
---
+++ part3/notes-backend/models/note.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const url = process.env.DATABASE_URI;
+
+console.log('connecting to ', url);
+
+mongoose
+	.connect(url)
+	.then((res) => console.log('connected to MongoDB'))
+	.catch((err) => {
+		console.log('An error occured connecting to MongoDB:', err.message);
+	});
+
+const noteSchema = mongoose.Schema({
+	content: String,
+	date: Date,
+	important: Boolean,
+});
+
+noteSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	},
+});
+
+module.exports = mongoose.model('Note', noteSchema);
