d91d3334b5ca6018a9f0235a1fa4b5a8919e6550
Sean <56064050+seangoats@users.noreply.github.com>
2022-02-11 05:39:23 +0800

    Complete 4.22

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,62 +1,76 @@
-const jwt = require('jsonwebtoken');
-const { SECRET } = require('../utils/config');
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const middleware = require('../utils/middleware');
+const asyncHandler = require('express-async-handler');
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
 	response.json(blogs);
 });
 
-blogsRouter.post('/', async (request, response) => {
-	const decodedToken = jwt.verify(request.token, SECRET);
-	if (!decodedToken.id) {
-		response.status(401).json({ error: 'missing or invalid token' });
-		return;
-	}
+blogsRouter.post(
+	'/',
+	middleware.userExtractor,
+	asyncHandler(async (request, response) => {
+		const user = request.user;
 
-	const user = await User.findById(decodedToken.id);
+		if (!user) {
+			response.status(400);
+		}
 
-	if (!request.body.title || !request.body.url) {
-		response.status(400).json({ error: 'Missing fields from blog! Please include all fields' });
-		return;
-	}
-	if (!request.body.likes) {
-		request.body.likes = 0;
-	}
+		if (!request.body.title || !request.body.url) {
+			response.status(400).json({ error: 'Missing fields from blog! Please include all fields' });
+			return;
+		}
+		if (!request.body.likes) {
+			request.body.likes = 0;
+		}
 
-	const blog = new Blog({ ...request.body, user: user._id });
-	const result = await blog.save();
-	user.blogs = user.blogs.concat(result._id);
-	await User.updateOne({ _id: user._id }, { blogs: user.blogs });
+		const blog = new Blog({ ...request.body, user: user._id });
+		const result = await blog.save();
+		user.blogs = user.blogs.concat(result._id);
+		await User.updateOne({ _id: user._id }, { blogs: user.blogs });
 
-	return response.status(201).json(result);
-});
+		return response.status(201).json(result);
+	})
+);
 
-blogsRouter.delete('/:id', async (request, response) => {
-	const decodedToken = jwt.verify(request.token, SECRET);
-	if (!decodedToken.id) {
-		response.status(401).json({ error: 'missing or invalid token' });
-		return;
-	}
+blogsRouter.delete(
+	'/:id',
+	middleware.userExtractor,
+	asyncHandler(async (request, response) => {
+		const user = request.user;
+		const blog = await Blog.findById(request.params.id);
 
-	const blog = await Blog.findById(request.params.id);
+		if (!user) {
+			response.status(401).json('wrong user');
+			return;
+		}
 
-	if (blog.user.toString() !== decodedToken.id.toString()) {
-		response.status(401).json({ error: 'You are not allowed to delete this blog!' });
-	}
+		if (!blog) {
+			response.status(401).json('that blog does not exist');
+		}
 
-	await Blog.findByIdAndRemove(request.params.id);
-	response.status(204).end();
-});
+		if (blog.user.toString() !== user.id.toString()) {
+			response.status(401).json({ error: 'You are not allowed to delete this blog!' });
+			return;
+		}
 
-blogsRouter.put('/:id', async (request, response) => {
-	const blog = {
-		likes: request.body.likes,
-	};
-	const result = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
-	return response.status(204).json(result);
-});
+		await Blog.findByIdAndRemove(request.params.id);
+		response.status(204).end();
+	})
+);
+
+blogsRouter.put(
+	'/:id',
+	asyncHandler(async (request, response) => {
+		const blog = {
+			likes: request.body.likes,
+		};
+		const result = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+		return response.status(204).json(result);
+	})
+);
 
 module.exports = blogsRouter;
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,3 +1,8 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const { SECRET } = require('./config');
+const logger = require('./logger');
+
 const tokenExtractor = (request, response, next) => {
 	const authorization = request.get('authorization');
 	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
@@ -6,4 +11,31 @@ const tokenExtractor = (request, response, next) => {
 	next();
 };
 
-module.exports = { tokenExtractor };
+const userExtractor = async (request, response, next) => {
+	const decodedToken = jwt.verify(request.token, SECRET);
+	if (!decodedToken.id) {
+		response.status(401).json({ error: 'missing or invalid token' });
+		return;
+	}
+
+	request.user = await User.findById(decodedToken.id);
+	next();
+};
+
+const errorHandler = (error, request, response, next) => {
+	logger.error(error.message);
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' });
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message });
+	} else if (error.name === 'JsonWebTokenError') {
+		return response.status(401).json({
+			error: 'invalid token',
+		});
+	}
+
+	next(error);
+};
+
+module.exports = { tokenExtractor, userExtractor, errorHandler };
