ef9d981e34626d387bd582fdb1fa26fa62a07282
Sean <56064050+seangoats@users.noreply.github.com>
2022-02-11 04:22:07 +0800

    Complete 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const db = require('./utils/db');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 db.connect();
 
@@ -11,5 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 module.exports = app;
--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 
 const PORT = process.env.PORT;
 const DB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_DB_URI : process.env.DB_URI;
+const SECRET = process.env.SECRET;
 
 module.exports = {
 	PORT,
 	DB_URI,
+	SECRET,
 };
